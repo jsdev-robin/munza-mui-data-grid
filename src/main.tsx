c42978417b0b2ab2d6ef37fd7718da03fbce7325
jsdev-robin <jsdev.robin@gmail.com>
2026-07-31 23:44:47 +0600

    fix: update version in package.json to 1.0.2 and remove unused index.css import

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
 
 const theme = createTheme({
   palette: {
