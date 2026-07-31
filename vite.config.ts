c9588ba4a1c2519d37d19085c8f664ba12a0ca4b
jsdev-robin <jsdev.robin@gmail.com>
2026-07-31 23:01:42 +0600

    feat: update package.json and pnpm-lock.yaml for dependency management, enhance tsconfig for path resolution, and configure vite for library build

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,39 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import babel from '@rolldown/plugin-babel';
+import react, { reactCompilerPreset } from '@vitejs/plugin-react';
+import { resolve } from 'node:path';
+import path from 'path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
+    dts({
+      tsconfigPath: './tsconfig.app.json',
+    }),
   ],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'MunzaMuiDataGrid',
+      fileName: 'index',
+      formats: ['es', 'cjs'],
+    },
+    rolldownOptions: {
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+});
